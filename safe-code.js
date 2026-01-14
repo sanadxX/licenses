5ce340200c28f0edd07fb833fa5541e668287a29
sanadxX <brawlstarzsnd@gmail.com>
2026-01-14 16:26:08 +0300

    Delete safe-code.js

--- safe-code.js
+++
@@ -1,12 +0,0 @@
-console.log("🟢 الكود بدأ تنفيذه داخل sandbox");
-
-// نستخدم المتغير playerId والدالة sendMessageToPlayer من البيئة المضيفة
-console.log("معرّف اللاعب:", playerId);
-
-sendMessageToPlayer(playerId, "مرحباً بك في السيرفر! 👋");
-
-// مثال بسيط لتعديل متغير (يمكنك توسيعه حسب حاجتك)
-const serverData = { score: 10 };
-serverData.score += 5;
-
-console.log("النقاط الجديدة للاعب:", serverData.score);
